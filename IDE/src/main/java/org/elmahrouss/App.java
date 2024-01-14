942879c1b975984bbf7890f24a08ed77d3226054
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-01-14 12:36:21 +0100

    App.java: remove unused import.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- IDE/src/main/java/org/elmahrouss/App.java
+++ IDE/src/main/java/org/elmahrouss/App.java
@@ -3,7 +3,6 @@ package org.elmahrouss;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
