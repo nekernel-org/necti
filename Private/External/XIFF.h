44b01cbcb08b5a4f58cf4eb8ac9b3b73f9741fc9
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-03-01 17:48:35 +0100

    SDK: Update XIFF container.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- Private/External/XIFF.h
+++ Private/External/XIFF.h
@@ -17,7 +17,7 @@
  */
 
 struct __attribute__((packed)) XiffHeader {
-  unsigned char mag[5];      // XIFF string (includes \0)
+  unsigned char mag[4];      // XIFF string (includes \0)
   unsigned int size;         // overall size of header (XiffHeader) in bytes
   unsigned int format_type;  // format type. generic
   unsigned char specific_mag[4];      // The sub header magic
