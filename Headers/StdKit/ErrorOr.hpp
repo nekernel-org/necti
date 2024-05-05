8ca205395dc7d76fecd2baf8d6b59d7df01825fd
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-05-05 21:09:20 +0200

    MHR-24: Add run_format.sh
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- Headers/StdKit/ErrorOr.hpp
+++ Headers/StdKit/ErrorOr.hpp
@@ -12,34 +12,50 @@
 #include <Headers/Defines.hpp>
 #include <Headers/StdKit/Ref.hpp>
 
-namespace CompilerKit {
-using ErrorT = UInt32;
+namespace CompilerKit
+{
+	using ErrorT = UInt32;
 
-template <typename T>
-class ErrorOr final {
- public:
-  ErrorOr() = default;
-  ~ErrorOr() = default;
+	template <typename T>
+	class ErrorOr final
+	{
+	public:
+		ErrorOr()  = default;
+		~ErrorOr() = default;
 
- public:
-  explicit ErrorOr(Int32 err) : mId(err) {}
+	public:
+		explicit ErrorOr(Int32 err)
+			: mId(err)
+		{
+		}
 
-  explicit ErrorOr(nullPtr Null) {}
+		explicit ErrorOr(nullPtr Null)
+		{
+		}
 
-  explicit ErrorOr(T Class) : mRef(Class) {}
+		explicit ErrorOr(T Class)
+			: mRef(Class)
+		{
+		}
 
-  ErrorOr &operator=(const ErrorOr &) = default;
-  ErrorOr(const ErrorOr &) = default;
+		ErrorOr& operator=(const ErrorOr&) = default;
+		ErrorOr(const ErrorOr&)			   = default;
 
-  Ref<T> Leak() { return mRef; }
+		Ref<T> Leak()
+		{
+			return mRef;
+		}
 
-  operator bool() { return mRef; }
+		operator bool()
+		{
+			return mRef;
+		}
 
- private:
-  Ref<T> mRef;
-  Int32 mId{0};
-};
+	private:
+		Ref<T> mRef;
+		Int32  mId{0};
+	};
 
-using ErrorOrAny = ErrorOr<voidPtr>;
+	using ErrorOrAny = ErrorOr<voidPtr>;
 
-}  // namespace CompilerKit
+} // namespace CompilerKit
--- Headers/StdKit/PEF.hpp
+++ Headers/StdKit/PEF.hpp
@@ -11,14 +11,14 @@
 // @file PEF.hpp
 // @brief Preferred Executable Format
 
-#define kPefMagic "Joy!"
+#define kPefMagic	 "Joy!"
 #define kPefMagicFat "yoJ!"
 
-#define kPefExt ".exec"
-#define kPefDylibExt ".lib"
-#define kPefLibExt ".slib"
+#define kPefExt		  ".exec"
+#define kPefDylibExt  ".lib"
+#define kPefLibExt	  ".slib"
 #define kPefObjectExt ".obj"
-#define kPefDebugExt ".dbg"
+#define kPefDebugExt  ".dbg"
 
 #define kPefMagicLen 5
 
@@ -29,93 +29,104 @@
 
 #define kPefStart "__ImageStart"
 
-namespace CompilerKit {
-enum {
-  kPefArchIntel86S = 100,
-  kPefArchAMD64,
-  kPefArchRISCV,
-  kPefArch64000, /* 64x0 RISC architecture. */
-  kPefArch32000,
-  kPefArchPowerPC, /* 64-bit POWER architecture. */
-  kPefArchCount = (kPefArchPowerPC - kPefArchIntel86S) + 1,
-  kPefArchInvalid = 0xFF,
-};
+namespace CompilerKit
+{
+	enum
+	{
+		kPefArchIntel86S = 100,
+		kPefArchAMD64,
+		kPefArchRISCV,
+		kPefArch64000, /* 64x0 RISC architecture. */
+		kPefArch32000,
+		kPefArchPowerPC, /* 64-bit POWER architecture. */
+		kPefArchCount	= (kPefArchPowerPC - kPefArchIntel86S) + 1,
+		kPefArchInvalid = 0xFF,
+	};
 
-enum {
-  kPefSubArchAMD,
-  kPefSubArchIntel,
-  kPefSubArchARM,
-  kPefSubArchIBM,
-};
+	enum
+	{
+		kPefSubArchAMD,
+		kPefSubArchIntel,
+		kPefSubArchARM,
+		kPefSubArchIBM,
+	};
 
-enum {
-  kPefKindExec = 1,         /* .exe */
-  kPefKindSharedObject = 2, /* .lib */
-  kPefKindObject = 4,       /* .obj */
-  kPefKindDebug = 5,        /* .dbg */
-  kPefKindDriver = 6,
-  kPefKindCount,
-};
+	enum
+	{
+		kPefKindExec		 = 1, /* .exe */
+		kPefKindSharedObject = 2, /* .lib */
+		kPefKindObject		 = 4, /* .obj */
+		kPefKindDebug		 = 5, /* .dbg */
+		kPefKindDriver		 = 6,
+		kPefKindCount,
+	};
 
-/* PEF container */
-typedef struct PEFContainer final {
-  CharType Magic[kPefMagicLen];
-  UInt32 Linker;
-  UInt32 Version;
-  UInt32 Kind;
-  UInt32 Abi;
-  UInt32 Cpu;
-  UInt32 SubCpu;  /* Cpu specific information */
-  UIntPtr Start;  /* Origin of code */
-  SizeType HdrSz; /* Size of header */
-  SizeType Count; /* container header count */
-} PACKED PEFContainer;
+	/* PEF container */
+	typedef struct PEFContainer final
+	{
+		CharType Magic[kPefMagicLen];
+		UInt32	 Linker;
+		UInt32	 Version;
+		UInt32	 Kind;
+		UInt32	 Abi;
+		UInt32	 Cpu;
+		UInt32	 SubCpu; /* Cpu specific information */
+		UIntPtr	 Start;	 /* Origin of code */
+		SizeType HdrSz;	 /* Size of header */
+		SizeType Count;	 /* container header count */
+	} PACKED PEFContainer;
 
-/* First PEFCommandHeader starts after PEFContainer */
-/* Last container is __exec_end */
+	/* First PEFCommandHeader starts after PEFContainer */
+	/* Last container is __exec_end */
 
-/* PEF executable section and commands. */
+	/* PEF executable section and commands. */
 
-typedef struct PEFCommandHeader final {
-  CharType Name[kPefNameLen]; /* container name */
-  UInt32 Cpu;             /* container cpu */
-  UInt32 SubCpu;          /* container sub-cpu */
-  UInt32 Flags;               /* container flags */
-  UInt16 Kind;                /* container kind */
-  UIntPtr Offset;             /* file offset */
-  SizeType Size;              /* file size */
-} PACKED PEFCommandHeader;
+	typedef struct PEFCommandHeader final
+	{
+		CharType Name[kPefNameLen]; /* container name */
+		UInt32	 Cpu;				/* container cpu */
+		UInt32	 SubCpu;			/* container sub-cpu */
+		UInt32	 Flags;				/* container flags */
+		UInt16	 Kind;				/* container kind */
+		UIntPtr	 Offset;			/* file offset */
+		SizeType Size;				/* file size */
+	} PACKED PEFCommandHeader;
 
-enum {
-  kPefCode = 0xC,
-  kPefData = 0xD,
-  kPefZero = 0xE,
-  kPefLinkerID = 0x1,
-  kPefCount = 4,
-  kPefInvalid = 0xFF,
-};
-}  // namespace CompilerKit
+	enum
+	{
+		kPefCode	 = 0xC,
+		kPefData	 = 0xD,
+		kPefZero	 = 0xE,
+		kPefLinkerID = 0x1,
+		kPefCount	 = 4,
+		kPefInvalid	 = 0xFF,
+	};
+} // namespace CompilerKit
 
-inline std::ofstream& operator<<(std::ofstream& fp,
-                                 CompilerKit::PEFContainer& container) {
-  fp.write((char*)&container, sizeof(CompilerKit::PEFContainer));
-  return fp;
+inline std::ofstream& operator<<(std::ofstream&				fp,
+								 CompilerKit::PEFContainer& container)
+{
+	fp.write((char*)&container, sizeof(CompilerKit::PEFContainer));
+	return fp;
 }
 
-inline std::ofstream& operator<<(std::ofstream& fp,
-                                 CompilerKit::PEFCommandHeader& container) {
-  fp.write((char*)&container, sizeof(CompilerKit::PEFCommandHeader));
-  return fp;
+inline std::ofstream& operator<<(std::ofstream&					fp,
+								 CompilerKit::PEFCommandHeader& container)
+{
+	fp.write((char*)&container, sizeof(CompilerKit::PEFCommandHeader));
+	return fp;
 }
 
-std::ifstream& operator>>(std::ifstream& fp,
-                          CompilerKit::PEFContainer& container) {
-  fp.read((char*)&container, sizeof(CompilerKit::PEFContainer));
-  return fp;
+std::ifstream& operator>>(std::ifstream&			 fp,
+						  CompilerKit::PEFContainer& container)
+{
+	fp.read((char*)&container, sizeof(CompilerKit::PEFContainer));
+	return fp;
 }
 
-std::ifstream& operator>>(std::ifstream& fp,
-                          CompilerKit::PEFCommandHeader& container) {
-  fp.read((char*)&container, sizeof(CompilerKit::PEFCommandHeader));
-  return fp;
+std::ifstream& operator>>(std::ifstream&				 fp,
+						  CompilerKit::PEFCommandHeader& container)
+{
+	fp.read((char*)&container, sizeof(CompilerKit::PEFCommandHeader));
+	return fp;
 }
