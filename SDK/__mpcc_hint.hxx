8b4e2d599909952d805d05445d98b82383622e66
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-05-06 14:14:07 +0200

    MHR-21: Add HintKit header.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

---
+++ SDK/__mpcc_hint.hxx
@@ -0,0 +1,20 @@
+/* -------------------------------------------
+
+    Copyright Mahrouss Logic
+
+------------------------------------------- */
+
+#pragma once
+
+#pragma compiler(hint_manifest)
+
+#define _Input
+#define _Output
+
+#define _Optional
+
+#define _StrictCheckInput
+#define _StrictCheckOutput
+
+#define _InOut
+#define _StrictInOut
