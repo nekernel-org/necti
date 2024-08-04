2efa8712a9d116e9ca891acec686a5158916d3d2
Amlal EL Mahrouss <amlalelmahrouss@icloud.com>
2024-08-04 12:17:24 +0200

    [BUMP] bumping.
    Signed-off-by: Amlal EL Mahrouss <amlalelmahrouss@icloud.com>

---
+++ NDKKit/Private/Imaging/TPM.hxx
@@ -0,0 +1,17 @@
+/* -------------------------------------------
+
+	Copyright ZKA Technologies
+
+------------------------------------------- */
+
+/**
+	@brief The Explicit Partition Map scheme.
+*/
+
+#ifndef __IMAGING_TPM_HXX__
+#define __IMAGING_TPM_HXX__
+
+struct TPM_IMAGE_HEADER;
+struct TPM_FILE_RECORD;
+
+#endif // ifndef __IMAGING_TPM_HXX__
