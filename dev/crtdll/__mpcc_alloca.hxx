ca64c5bf12b583e4a170ab34fd5002b8c069106e
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-08-20 19:14:43 +0200

    [IMP] Moved SDK/CRT into it's own directory in /dev/crtdll.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- dev/crtdll/__mpcc_alloca.hxx
+++ dev/crtdll/__mpcc_alloca.hxx
@@ -9,7 +9,7 @@
 typedef void*		  ptr_type;
 typedef __SIZE_TYPE__ size_type;
 
-inline void* __mpcc_alloca_gcc(size_type sz)
+inline ptr_type __mpcc_alloca_gcc(size_type sz)
 {
 	return __builtin_alloca(sz);
 }
