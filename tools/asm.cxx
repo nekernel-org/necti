0baeaa7bd4ed71f150124c4808827e1903983a8e
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-08-30 13:15:04 +0200

    [IMP] Improve assembler frontend.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- tools/asm.cxx
+++ tools/asm.cxx
@@ -10,13 +10,27 @@
 #include <ndkdll/Version.hxx>
 #include <iostream>
 #include <cstring>
+#include <string>
 #include <vector>
 
+extern "C" int ZKAAssemblerMainPowerPC(int argc, char const* argv[]);
 extern "C" int ZKAAssemblerMain64000(int argc, char const* argv[]);
+extern "C" int ZKAAssemblerMainAMD64(int argc, char const* argv[]);
 
 int main(int argc, char const* argv[])
 {
-	for (size_t index_arg = 0; index_arg < argc; ++index_arg)
+	std::vector<const char*> arg_vec_cstr;
+	arg_vec_cstr.push_back(argv[0]);
+
+	enum
+	{
+		eX64Assembler,
+		e64X0Assembler,
+		ePOWER64Assembler,
+		eInvalidAssembler
+	} cAsm = eInvalidAssembler;
+
+	for (size_t index_arg = 1; index_arg < argc; ++index_arg)
 	{
 		if (strstr(argv[index_arg], "/?"))
 		{
@@ -27,12 +41,53 @@ int main(int argc, char const* argv[])
 
 			return 0;
 		}
+		else if (strstr(argv[index_arg], "/Asm:x64"))
+		{
+			cAsm = eX64Assembler;
+		}
+		else if (strstr(argv[index_arg], "/Asm:64x0"))
+		{
+			cAsm = e64X0Assembler;
+		}
+		else if (strstr(argv[index_arg], "/Asm:POWER64"))
+		{
+			cAsm = ePOWER64Assembler;
+		}
+		else
+		{
+			arg_vec_cstr.push_back(argv[index_arg]);
+		}
 	}
 
-	if (int32_t code = ZKAAssemblerMain64000(argc, argv); code)
+	switch (cAsm)
 	{
-		std::printf("asm.exe: frontend exited with code %i.\n", code);
-		return code;
+	case ePOWER64Assembler: {
+		if (int32_t code = ZKAAssemblerMainPowerPC(arg_vec_cstr.size(), arg_vec_cstr.data()); code)
+		{
+			std::printf("asm.exe: frontend exited with code %i.\n", code);
+			return code;
+		}
+		break;
+	}
+	case e64X0Assembler: {
+		if (int32_t code = ZKAAssemblerMain64000(arg_vec_cstr.size(), arg_vec_cstr.data()); code)
+		{
+			std::printf("asm.exe: frontend exited with code %i.\n", code);
+			return code;
+		}
+		break;
+	}
+	case eX64Assembler: {
+		if (int32_t code = ZKAAssemblerMainAMD64(arg_vec_cstr.size(), arg_vec_cstr.data()); code)
+		{
+			std::printf("asm.exe: frontend exited with code %i.\n", code);
+			return code;
+		}
+		break;
+	}
+	default: {
+		return -1;
+	}
 	}
 
 	return 0;
