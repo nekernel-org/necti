affddb700e1a12a96323b59c2a41f870ccbe45a9
Amlal EL Mahrouss <amlalelmahrouss@icloud.com>
2024-10-06 11:13:33 +0200

    IMP: Add exit.hxx to expose exit function.
    Signed-off-by: Amlal EL Mahrouss <amlalelmahrouss@icloud.com>

---
+++ dev/crt/exit.hxx
@@ -0,0 +1,21 @@
+/* -------------------------------------------
+
+	Copyright ZKA Technologies.
+
+------------------------------------------- */
+
+#pragma once
+
+/// @brief CRT exit, with exit code (!!! exits all threads. !!!)
+/// @param code
+/// @return
+extern "C" int exit(int code);
+
+/// @brief Standard C++ namespace
+namespace std
+{
+	inline int exit(int code)
+	{
+		exit(code);
+	}
+} // namespace std
