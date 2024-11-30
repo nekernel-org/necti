0aeecbbeebf1a34abf146c6191162bb645d56188
Amlal <amlalelmahrouss@icloud.com>
2024-11-30 20:03:20 +0100

    Fix copyright holder.

--- dev/ToolchainKit/src/CPlusPlusRuleChecker.cc
+++ dev/ToolchainKit/src/CPlusPlusRuleChecker.cc
@@ -2,7 +2,7 @@
  *	========================================================
  *
  *	CPlusPlusRuleChecker
- * 	Copyright (C) 2024, EL Mahrouss Logic, all rights reserved.
+ * 	Copyright (C) 2024, ELMH Group, all rights reserved.
  *
  * 	========================================================
  */
