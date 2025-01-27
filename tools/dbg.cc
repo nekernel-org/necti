6fadc89e56965342b37c4476ec17c604c1be3412
Amlal EL Mahrouss <113760121+amlel-el-mahrouss@users.noreply.github.com>
2025-01-27 10:07:24 +0100

    Refactor dbg.cc CLI tool.

--- tools/dbg.cc
+++ tools/dbg.cc
@@ -3,111 +3,115 @@
  */
 
 #include <iostream>
+#include <unordered_map>
+
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/user.h>
 #include <unistd.h>
-#include <unordered_map>
 #include <stdint.h>
 
-namespace Dbg
+namespace LibDebugger
 {
-	/// \brief Debug Interface class in C++
+	/// \brief Debug IDebugger class in C++
 	/// \author Amlal El Mahrouss
-	class Interface final
+	class IDebugger final
 	{
 	public:
-		Interface()	 = default;
-		~Interface() = default;
+		IDebugger()	 = default;
+		~IDebugger() = default;
 
-		Interface& operator=(const Interface&) = default;
-		Interface(const Interface&)			   = default;
+		IDebugger& operator=(const IDebugger&) = default;
+		IDebugger(const IDebugger&)			   = default;
 
+	public:
 		void Attach(pid_t pid)
 		{
-			this->pid = pid;
+			this->m_pid = pid;
 
-			if (ptrace(PTRACE_ATTACH, this->pid, nullptr, nullptr) == -1)
+			if (ptrace(PTRACE_ATTACH, this->m_pid, nullptr, nullptr) == -1)
 			{
-				perror("DBG: Attach");
+				perror("dbg: Attach");
 				exit(1);
 			}
 
-			waitpid(pid, nullptr, 0);
-			std::cout << "[+] Attached to process: " << pid << std::endl;
+			waitpid(m_pid, nullptr, 0);
+			
+			std::cout << "[+] Attached to process: " << m_pid << std::endl;
 		}
 
 		void SetBreakpoint(void* addr)
 		{
-			long original_data = ptrace(PTRACE_PEEKTEXT, pid, addr, nullptr);
+			long original_data = ptrace(PTRACE_PEEKTEXT, m_pid, addr, nullptr);
 			if (original_data == -1)
 			{
-				perror("DBG: Peek");
+				perror("dbg: Peek");
 				exit(1);
 			}
 
 			long data_with_int3 = (original_data & ~0xFF) | 0xCC; // Insert INT3 (0xCC)
-			if (ptrace(PTRACE_POKETEXT, pid, addr, data_with_int3) == -1)
+			if (ptrace(PTRACE_POKETEXT, m_pid, addr, data_with_int3) == -1)
 			{
-				perror("DBG: Poke");
+				perror("dbg: Poke");
 				exit(1);
 			}
 
 			std::cout << "[+] Breakpoint set at: " << addr << std::endl;
-			breakpoints[reinterpret_cast<uintptr_t>(addr)] = original_data; // Store original data
+
+			m_breakpoints[reinterpret_cast<uintptr_t>(addr)] = original_data; // Store original data
 		}
 
 		void ContinueExecution()
 		{
-			if (ptrace(PTRACE_CONT, pid, nullptr, nullptr) == -1)
+			if (ptrace(PTRACE_CONT, m_pid, nullptr, nullptr) == -1)
 			{
-				perror("DBG: Cont");
+				perror("dbg: Cont");
 				exit(1);
 			}
 
 			int status;
-			waitpid(pid, &status, 0);
+			waitpid(m_pid, &status, 0);
 
 			if (WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP)
 			{
-				std::cout << "[!] Breakpoint hit!" << std::endl;
+				std::cout << "[!] Breakpoint hit." << std::endl;
 			}
 		}
 
 		void Detach()
 		{
-			if (ptrace(PTRACE_DETACH, pid, nullptr, nullptr) == -1)
+			if (ptrace(PTRACE_DETACH, m_pid, nullptr, nullptr) == -1)
 			{
-				perror("DBG: Detach");
+				perror("dbg: Detach");
 				exit(1);
 			}
 
-			std::cout << "[-] Detached from process: " << pid << std::endl;
+			std::cout << "[-] Detached from process: " << m_pid << std::endl;
 		}
 
 		std::unordered_map<uintptr_t, long>& Breakpoints()
 		{
-			return breakpoints;
+			return m_breakpoints;
 		}
 
 	private:
-		pid_t								pid;
-		std::unordered_map<uintptr_t, long> breakpoints;
+		pid_t								m_pid;
+		std::unordered_map<uintptr_t, long> m_breakpoints;
 	};
-} // namespace Dbg
+} // namespace LibDebugger
 
 int main(int argc, char* argv[])
 {
 	if (!argv[1])
 	{
-		std::cout << "[?] Attach to process using it's PID.\n";
+		std::cout << "[?] Enter a PID to attach on.\n";
 		return EXIT_FAILURE;
 	}
 
 	pid_t pid = std::stoi(argv[1]);
 
-	Dbg::Interface debugger;
+	LibDebugger::IDebugger debugger;
 	debugger.Attach(pid);
 
 	while (true)
@@ -140,9 +144,10 @@ int main(int argc, char* argv[])
 		if (cmd == "b" ||
 			cmd == "break")
 		{
-			std::cout << "[?] Enter an address to break on: ";
+			std::cout << "[?] Enter an address to add a breakpoint on: ";
 
 			std::getline(std::cin, cmd);
+
 			void* breakpoint_addr = reinterpret_cast<void*>(std::stoul(cmd.c_str(), nullptr, 16));
 
 			if (breakpoint_addr)
