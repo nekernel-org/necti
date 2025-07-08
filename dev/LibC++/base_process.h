ffad2caaf9daf540e313f2ffe029de04cb3d2b80
Amlal El Mahrouss <amlal@nekernel.org>
2025-07-08 18:31:22 +0200

    fix: LibC++: use `exit_` symbol instead of `exit`
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- dev/LibC++/base_process.h
+++ dev/LibC++/base_process.h
@@ -9,12 +9,12 @@
 /// @brief CRT exit, with exit code (!!! exits all threads. !!!)
 /// @param code the exit code.
 /// @return the return > 0 for non successful.
-extern "C" int exit(int code);
+extern "C" int exit_(int code);
 
 /// @brief Standard C++ namespace
 namespace std::base_process {
 inline int exit(int code) {
-  exit(code);
+  exit_(code);
   return -1;
 }
 }  // namespace std::base_process
