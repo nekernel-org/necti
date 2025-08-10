fde73a5d4127938c8b84bdb1907abb232eef56cb
Amlal <amlal@nekernel.org>
2025-08-10 10:31:27 +0200

    fix: tools: fix compiler errors caused by codebase refactors.
    Signed-off-by: Amlal <amlal@nekernel.org>

--- tools/pef-amd64-cxxdrv.cc
+++ tools/pef-amd64-cxxdrv.cc
@@ -17,7 +17,7 @@ static auto kPath   = "/usr/local/lib/libCompilerKit.dylib";
 static auto kSymbol = "CompilerCPlusPlusAMD64";
 
 Int32 main(Int32 argc, Char const* argv[]) {
-  CompilerKitDylib handler = dlopen(kPath, RTDK_LAZY | RTDK_GLOBAL);
+  CompilerKitDylib handler = dlopen(kPath, RTLD_LAZY | RTLD_GLOBAL);
 
   if (!handler) {
     kStdOut;
