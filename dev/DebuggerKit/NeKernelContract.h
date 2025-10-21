283d25d583a62aa9e56a2af412a7074e31db3069
Amlal El Mahrouss <amlal@nekernel.org>
2025-10-21 04:02:52 +0200

    fix: debugger_kit: fix protocol magic.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- dev/DebuggerKit/NeKernelContract.h
+++ dev/DebuggerKit/NeKernelContract.h
@@ -19,7 +19,7 @@ class NeKernelContract;
 namespace Detail {
   inline constexpr auto     kDebugCmdLen  = 256U;
   inline constexpr auto     kDebugPort    = 51820;
-  inline constexpr auto     kDebugMagic   = "VMK1.0.0;";
+  inline constexpr auto     kDebugMagic   = "NE1.0.0;";
   inline constexpr uint16_t kDebugVersion = 0x0100;
   inline constexpr auto     kDebugDelim   = ';';
   inline constexpr auto     kDebugEnd     = '\r';
