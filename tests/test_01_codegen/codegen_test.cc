ad4deb740e8a358542d59dae2b3c1225c02b50db
Amlal El Mahrouss <amlal@nekernel.org>
2025-11-26 22:58:06 -0500

    chore: tests: updating codegen unit tests for codegen.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ tests/test_01_codegen/codegen_test.cc
@@ -0,0 +1,14 @@
+/* ========================================
+
+   Copyright (C) 2024-2025 Amlal El Mahrouss, Licensed under the Apache 2.0 license
+
+   ======================================== */
+
+/// @brief Codegen Unit test, from the C++ unit to the final executable.
+/// @author Amlal El Mahrouss
+
+#include <gtest/gtest.h>
+
+TEST(CodegenTest, BasicCodegenTest) {
+  
+}
