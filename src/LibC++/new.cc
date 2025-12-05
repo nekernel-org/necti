037ac38824623c13070384e8fc0e70c4770dcdbd
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-05 11:49:28 -0500

    chore! new project filesystem structure.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- src/LibC++/new.cc
+++
@@ -1,7 +0,0 @@
-/* ========================================
-
-  Copyright (C) 2025, Amlal El Mahrouss, licensed under the Apache 2.0 license.
-
-======================================== */
-
-#include <LibC++/new.h>
