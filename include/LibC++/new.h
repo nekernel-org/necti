6cc0c03a0b66ea55b246a86639a5329e4aac05dc
Amlal El Mahrouss <amlal@nekernel.org>
2025-12-19 11:01:41 +0100

    feat: Add `placeable_callable_type` alias.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/LibC++/new.h
+++ include/LibC++/new.h
@@ -39,8 +39,12 @@ void* operator new[](size_t);
 
 /// \brief placement_t new and delete operators. Governs how the memory shall be placed.
 /// \note This is a feature that shall be used wisely, failure to do so will produce Undefined Behaviors at runtime.
-void* operator _placement_new(size_t);
-void operator _placement_delete(void*);
+void* operator _placement_new(struct placement_t*);
+void operator _placement_delete(struct placement_t*, void*);
+
+/// \brief For all offsets within the base range and alignement 'align'
+/// \brief Allocate offsets with respect to the `base` interval, apply alignement of `align` value. Return `offsets` of length n as an aligned value within the domain of `base`.
+using placeable_callable_type = void*/*offsets*/(*)(void* base, size_t n, const int& align);
 
 /// \note This should NOT fail, failure to meet the conditions will cause the program's state to be aborted.
 /// \brief Set the placement policy of future memory allocations.
