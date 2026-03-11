df7ef3ba6105cc414b69f2b0666d50dec7030536
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-11 06:09:44 +0100

    [CHORE] Update GenericsLibrary include library.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ include/CoreRuntimeKit/Nectar/ncl_exports.h
@@ -0,0 +1,6 @@
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (See accompanying
+// file LICENSE or copy at http://www.apache.org/licenses/LICENSE-2.0)
+// Official repository: https://github.com/ne-foss-org/nectar
+
+#pragma once
