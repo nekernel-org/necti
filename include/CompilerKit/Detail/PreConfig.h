fe55f67bf7a5aeba365d7af1d681cef1d9ed7a3c
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-12 18:23:31 +0100

    [CHORE] Codebase format cleanup.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- include/CompilerKit/Detail/PreConfig.h
+++ include/CompilerKit/Detail/PreConfig.h
@@ -87,7 +87,8 @@
 #define kObjectFileExt ".obj"
 #define kBinaryFileExt ".bin"
 
-#define kAsmFileExts {".64x", ".32x", ".masm", ".s", ".S", ".asm", ".x64"}
+#define kAsmFileExts \
+  { ".64x", ".32x", ".masm", ".s", ".S", ".asm", ".x64" }
 
 #define kAsmFileExtsMax (7U)
 
