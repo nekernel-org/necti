13a1c3191cee5adf4a91c16c3c1926b9df7596f9
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-23 10:23:09 +0100

    [FIX] Fix install script.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ nebuild posix/cppdrv.json posix/ld64-posix.json posix/pef-amd64-asm.json posix/p
 sudo cp cppdrv /usr/local/bin/cppdrv-nectar
 sudo cp ld64 /usr/local/bin/ld64-nectar
 sudo cp pef-amd64-asm /usr/local/bin/
-sudo cp pef-amd64-drv /usr/local/bin/
-sudo cp ptx-drv /usr/local/bin/
+sudo cp pef-amd64-necdrv /usr/local/bin/
+sudo cp ptx-necdrv /usr/local/bin/
 
 echo "== WELCOME TO NECTAR =="
 echo "== HELP =="
