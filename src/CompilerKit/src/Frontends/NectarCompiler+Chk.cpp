c241e7582926b8455f0be469c6c6505d3fb0250d
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-24 07:11:06 +0100

    [FIX] CompilerKit: Fix syntax checker rule.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- src/CompilerKit/src/Frontends/NectarCompiler+Chk.cpp
+++ src/CompilerKit/src/Frontends/NectarCompiler+Chk.cpp
@@ -48,8 +48,8 @@ CK_IMPORT_C bool NectarCheckFrontend(CompilerKit::STLString& input) {
     }
   }
 
-  if (input.find("(") != CompilerKit::STLString::npos && !input.ends_with(";")) {
-    if (input.find(":=") == CompilerKit::STLString::npos) {
+  if (input.find("(") != CompilerKit::STLString::npos) {
+    if (input.find(";") == CompilerKit::STLString::npos) {
       Detail::print_error("A function call must always end with ';'", "check");
       return false;
     }
