612cf6123797127955c8595d837d4dd8c47ad6d0
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-26 17:11:42 +0100

    [CHORE] Fix the install script which seems to act funny.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ echo "INSTALLING NECTAR..."
 
 git clone -j8 https://github.com/ne-foss-org/nectar
 cd nectar
+
+sudo cp -r include/GenericsLibrary /usr/local/include/
+chmod +x share/bin/nectar
+sudo cp share/bin/nectar /usr/local/bin/
+
 cd src/CompilerKit
 sudo nebuild ck-posix.json
 cd ..
@@ -11,16 +16,12 @@ cd DebuggerKit
 sudo nebuild dk-nk-posix.json
 cd ..
 cd CommandLine
-nebuild posix/cppdrv.json posix/ld64-posix.json posix/pef-amd64-asm.json posix/pef-amd64-drv.json posix/ptx-drv.json
+nebuild posix/ld64-posix.json posix/pef-amd64-asm.json posix/pef-amd64-drv.json posix/ptx-drv.json
 #sudo cp cppdrv /usr/local/bin/cppdrv-nectar
 sudo cp ld64 /usr/local/bin/ld64-nectar
 sudo cp pef-amd64-asm /usr/local/bin/
 sudo cp pef-amd64-necdrv /usr/local/bin/
 sudo cp ptx-necdrv /usr/local/bin/
-cd ../../
-sudo cp -r include/GenericsLibrary /usr/local/include/
-chmod +x share/bin/nectar
-sudo cp share/bin/nectar /usr/local/bin/
 
 echo "== WELCOME TO NECTAR =="
 echo "== HELP =="
