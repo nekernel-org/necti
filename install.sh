08932f83c17fb4c57b708f04b19e21c4b915d135
Amlal <amlal@nekernel.org>
2026-04-18 13:24:06 +0200

    [CHORE] Add install.sh script by Makarov.

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
 #! /bin/sh
 
+# Author: Makarov
+# Imported-By: Amlal
+
 echo "==> WELCOME TO NECTAR."
 echo "==> INSTALLING NECTAR..."
 
-sudo apt update
-sudo apt install nasm build-essential cmake libboost-dev
+if command -v dnf >/dev/null 2>&1; then
+    sudo dnf groupinstall "Development Tools" -y
+    sudo dnf install cmake nasm boost-devel -y
+
+elif command -v yum >/dev/null 2>&1; then
+    sudo yum groupinstall "Development Tools"
+    sudo yum install nasm cmake boost-devel
+
+elif command -v pacman >/dev/null 2>&1; then
+    sudo pacman -Sy --noconfirm nasm base-devel cmake boost
+
+elif command -v zypper >/dev/null 2>&1; then
+    sudo zypper refresh
+    sudo zypper install -t pattern devel_basis
+    sudo zypper install nasm cmake boost-devel
+    
+else
+    echo "Unsupported distribution. Please check the readme and install dependencies manually."
+fi
+
+if [ $? -ne 0 ]; then
+    echo "Installation failed (sudo or package manager issue). Exiting."
+    exit 1
+fi
 
 git clone -j8 https://github.com/ne-foss/nectar
 cd nectar
